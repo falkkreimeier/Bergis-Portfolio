a6450df6f5ef32fdd7b0f8c452541d89d6b35a50
Falk Kreimeier <f.kreimeier@gmx.net>
2022-01-17 17:46:45 +0100

    delete unnecessary code

--- src/components/Menü.js
+++ src/components/Menü.js
@@ -74,64 +74,46 @@ const NavigationLink = styled(NavLink)`
   text-decoration: none;
   font-family: var(--hl-font)
   font-weight: 400;
-  font-size: 0.9rem;
   color: black;
   font-family: var(--hl-font);
   font-size: 0.9rem;
-  margin-bottom: 10px;
+  margin-bottom: 5px;
   padding: 5px;
-  width: 170px;
-  margin-bottom: 5px;    
+  width: 170px;    
   background-color: transparent;
   background-image: linear-gradient(to bottom, #fff, #f8eedb);
   border: 0 solid #e5e7eb;
   border-radius: 0.5rem;
   color: #482307;
-  column-gap: 1rem;
   cursor: pointer;
   outline: 2px solid transparent;
-  text-transform: none;
   transition: all 0.1s cubic-bezier(0.4, 0, 0.2, 1);
-  user-select: none;
-  -webkit-user-select: none;
   box-shadow: -6px 8px 10px rgba(81, 41, 10, 0.1),
     0px 2px 2px rgba(81, 41, 10, 0.2);
   }
   &.active {
-    /* text-decoration: underline; */
     opacity: 0.2;
-    color: black;
   }
 `;
 
 const NavigationLinkActive = styled(NavLink)`
   text-decoration: none;
-  font-family: var(--hl-font)
-  font-weight: 400;
-  font-size: 0.9rem;
   color: black;
   font-family: var(--hl-font);
   font-size: 0.9rem;
-  margin-bottom: 10px;
+  margin-bottom: 5px;
   padding: 5px;
   width: 170px;
-  margin-bottom: 5px;    
   background-color: transparent;
   background-image: linear-gradient(to bottom, #fff, #f8eedb);
   border: 0 solid #e5e7eb;
   border-radius: 0.5rem;
   opacity: 0.2;
-  color: black;
-  column-gap: 1rem;
   cursor: pointer;
   outline: 2px solid transparent;
-  text-transform: none;
   transition: all 0.1s cubic-bezier(0.4, 0, 0.2, 1);
-  user-select: none;
-  -webkit-user-select: none;
   box-shadow: -6px 8px 10px rgba(81, 41, 10, 0.1),
     0px 2px 2px rgba(81, 41, 10, 0.2);
-
 `;
 
 export default Menü;
